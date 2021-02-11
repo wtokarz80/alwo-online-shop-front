687fefc08e7a529255d7e21f4d3d602fe62b9115
Aldona <aldonapawlak2@gamil.com>
2021-02-12 00:03:02 +0100

    Add product details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
 import {MainComponent} from './main/main.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 const routes: Routes = [
   {path: 'sign-up', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'books', component: MainComponent}
+  {path: 'books', component: MainComponent},
+  {path: 'product/:id', component: ProductDetailsComponent}
 ];
 
 @NgModule({
