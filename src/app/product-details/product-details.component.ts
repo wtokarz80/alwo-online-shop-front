9417f9fd51b15880c7c130bb9ce83c5511455506
Aldona <aldonapawlak2@gamil.com>
2021-02-12 12:18:59 +0100

    Add Geowidget

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductService} from '../services/product.service';
 
@@ -8,11 +8,17 @@ import {ProductService} from '../services/product.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  @ViewChild('map')
+  map: ElementRef;
 
   constructor(private activatedRoute: ActivatedRoute, public productService: ProductService) { }
 
   ngOnInit(): void {
     const productId = this.activatedRoute.snapshot.paramMap.get('id');
     this.productService.getProductById(productId).subscribe();
+    // window.easyPackAsyncInit = () => {
+    setTimeout(() => this.map.nativeElement.init({}), 2000);
+      // this.map.nativeElement.init({});
+    // }
   }
 }
