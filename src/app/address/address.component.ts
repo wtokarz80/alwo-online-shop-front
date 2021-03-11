649dd2fed6eea9adc2445507b225e9f92886aa5f
wtokarz80 <w.tokarz80gmail.com>
2021-03-11 19:03:02 +0100

    added addressing from inpost object

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -5,12 +5,11 @@ import {Location} from '@angular/common';
 import {OrderService} from '../services/order.service';
 import {FormGroup, FormBuilder, FormArray, Validators, FormControl} from '@angular/forms';
 import {OrderStage} from '../models/orderStage';
-import {Shipment} from '../models/shipment';
-import {Payment} from '../models/payment';
-import {map} from 'rxjs/operators';
 import {BasketProductDto} from '../models/basketProductDto';
 import {BasketService} from '../services/basket.service';
 import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+import {Shipment} from '../models/shipment';
 
 @Component({
   selector: 'app-address',
@@ -24,13 +23,16 @@ export class AddressComponent implements OnInit, OnDestroy {
   nestedForm: FormGroup;
   orderStage: OrderStage;
   productCost: number;
+  isParcelLocker = false;
+
   private orderServiceGetStageSubscription: Subscription;
 
   constructor(private addressService: AddressService,
               private location: Location,
               public orderService: OrderService,
               private formBuilder: FormBuilder,
-              private basketService: BasketService) { }
+              private basketService: BasketService,
+              private router: Router) { }
 
   ngOnDestroy(): void {
         if (this.orderServiceGetStageSubscription) {
@@ -45,6 +47,7 @@ export class AddressComponent implements OnInit, OnDestroy {
     this.orderServiceGetStageSubscription = this.orderService.getStage().subscribe(
       data => {
         this.orderStage = data;
+        this.checkIfParcelLocker(data);
         this.fillForms();
       });
     this.basketService.getBasketProducts$().subscribe((data: BasketProductDto[]) => {
@@ -60,7 +63,8 @@ export class AddressComponent implements OnInit, OnDestroy {
       phone: ['', Validators.required],
       street: ['', Validators.required],
       apartmentNumber: ['', Validators.required],
-      zipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      // zipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      zipCode: ['', Validators.required],
       city: ['', Validators.required],
       description: [],
       contactType: [null, Validators.required]
@@ -81,10 +85,16 @@ export class AddressComponent implements OnInit, OnDestroy {
     if (this.addressArray.length > 1){
       this.addressArray.removeAt(index);
     }
+      for (let i = 0; i < this.orderStage.addresses.length; i++){
+        if (this.orderStage.addresses[i].firstName === 'Parcel locker'){
+          this.orderStage.addresses.splice(i, 1);
+          this.orderStage.shipment = {} as Shipment;
+          this.router.navigateByUrl('/basket');
+        }
+      }
   }
 
   submitHandler(): void {
-    this.addresses = [];
     for (const formGroup of this.addressArray.controls){
       const address = new Address();
       address.firstName = formGroup.get('firstName').value;
@@ -97,10 +107,15 @@ export class AddressComponent implements OnInit, OnDestroy {
       address.city = formGroup.get('city').value;
       address.description = formGroup.get('description').value;
       address.contactType = formGroup.get('contactType').value;
-      this.addresses.push(address);
+      if (this.orderStage.addresses && formGroup.get('firstName').value !== 'Parcel locker' && this.addresses.length < 2){
+        this.orderStage.addresses.push(address);
+      }
+      if (this.addresses.length < 2){
+        this.addresses.push(address);
+        this.orderStage.addresses = this.addresses;
+      }
     }
     // console.log({...this.nestedForm.value});
-    this.orderStage.addresses = this.addresses;
     this.orderService.setState(this.orderStage);
     console.log(this.orderStage);
   }
@@ -111,20 +126,24 @@ export class AddressComponent implements OnInit, OnDestroy {
       this.addAddress();
     }
     for (let i = 0; i < addresses.length; i++){
-      this.addressArray.controls[i].get('firstName').setValue(addresses[i].firstName);
-      this.addressArray.controls[i].get('lastName').setValue(addresses[i].lastName);
-      this.addressArray.controls[i].get('email').setValue(addresses[i].email);
-      this.addressArray.controls[i].get('phone').setValue(addresses[i].phone);
-      this.addressArray.controls[i].get('street').setValue(addresses[i].street);
-      this.addressArray.controls[i].get('apartmentNumber').setValue(addresses[i].apartmentNumber);
-      this.addressArray.controls[i].get('zipCode').setValue(addresses[i].zipCode);
-      this.addressArray.controls[i].get('city').setValue(addresses[i].city);
-      this.addressArray.controls[i].get('description').setValue(addresses[i].description);
-      this.addressArray.controls[i].get('contactType').setValue(addresses[i].contactType);
+      // if (!(addresses[i].firstName === 'Parcel locker')){
+        this.addressArray.controls[i].get('firstName').setValue(addresses[i].firstName);
+        this.addressArray.controls[i].get('lastName').setValue(addresses[i].lastName);
+        this.addressArray.controls[i].get('email').setValue(addresses[i].email);
+        this.addressArray.controls[i].get('phone').setValue(addresses[i].phone);
+        this.addressArray.controls[i].get('street').setValue(addresses[i].street);
+        this.addressArray.controls[i].get('apartmentNumber').setValue(addresses[i].apartmentNumber);
+        this.addressArray.controls[i].get('zipCode').setValue(addresses[i].zipCode);
+        this.addressArray.controls[i].get('city').setValue(addresses[i].city);
+        this.addressArray.controls[i].get('description').setValue(addresses[i].description);
+        this.addressArray.controls[i].get('contactType').setValue(addresses[i].contactType);
+      // }
     }
-    // addresses.forEach(address => {
-    //   this.addressArray.controls[0].get('firstName').setValue(address.firstName);
-    // });
-    console.log(addresses);
+  }
+
+  private checkIfParcelLocker(data: OrderStage): void {
+    if (data.shipment.shipmentMethod === 'Parcel locker'){
+      this.isParcelLocker = true;
+    }
   }
 }
